a2165fa20b186c554f4cdbada3218272b0a37ad2
Aravindh <124861577+Phoenix-070@users.noreply.github.com>
2025-12-13 14:46:59 +0530

    Removed api keys

--- extension/src/firebaseConfig.ts
+++ extension/src/firebaseConfig.ts
@@ -1,10 +1,7 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCj6qaPfaT8_1xhKFOO5qcYNtIh9Concag",
-    authDomain: "interactgen-voicetour.firebaseapp.com",
-    projectId: "interactgen-voicetour",
-    storageBucket: "interactgen-voicetour.firebasestorage.app",
-    messagingSenderId: "893679827888",
-    appId: "1:893679827888:web:24602f5875132c08bab475"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
 };
 
 import { initializeApp } from "firebase/app";
